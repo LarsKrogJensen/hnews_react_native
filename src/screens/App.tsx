1ef19ffe79b4161c27c7f8fc974848b9f32e81d9
Lars Krog-Jensen <lars.krogjensen@gmail.com>
2017-10-28 13:54:56 +0200

    added header images

--- src/screens/App.tsx
+++ src/screens/App.tsx
@@ -1,52 +1,37 @@
 import * as React from "react"
-import {Header, StackNavigator, TabNavigator} from "react-navigation"
+import {Header, NavigationStackScreenOptions, StackNavigator, TabNavigator} from "react-navigation"
 import HomeScreen from "screens/HomeScreen"
 import SportScreen from "screens/SportScreen";
 import BetHistoryScreen from "screens/BetHistoryScreen";
 import EventScreen from "screens/EventScreen";
 import LiveEventsScreen from "containers/LiveEventsScreen";
-import {Image, Platform, StyleSheet, View} from "react-native";
+import {Image, Platform, StatusBar, StyleSheet, View} from "react-native";
 import Icon from 'react-native-vector-icons/Ionicons';
 import {isIos} from "lib/platform";
 import absoluteFill = StyleSheet.absoluteFill;
 
-function iconResolver(icon: string) {
-    return ({focused, tintColor}) => {
-        let iconName = icon;
-        if (!focused) {
-            iconName += "-outline"
-        }
-
-        return <Icon name={iconName} size={30} color={tintColor}/>
-    }
+const defaultNavOptions: NavigationStackScreenOptions = {
+    headerTintColor: 'white',
+    headerTitleStyle: {color: '#fff'},
+    headerBackTitleStyle: {color: '#fff'},
+    header: (props) => <ImageHeader {...props} />
 }
-
-const ImageHeader = props => (
-    <View style={{backgroundColor: '#eee'}}>
-        <Image
-            style={absoluteFill}
-            source={{uri: 'https://upload.wikimedia.org/wikipedia/commons/3/36/Hopetoun_falls.jpg'}}
-        />
-        <Header {...props} style={{backgroundColor: 'transparent'}}/>
-    </View>
-);
-
 const HomeTab = StackNavigator({
     Home: {
         screen: HomeScreen,
         path: '/',
         navigationOptions: {
-            title: "Home",
-            headerTitleStyle: {color: '#fff'},
-            header: (props) => <ImageHeader {...props} />
+            ...defaultNavOptions,
+            title: "Home"
         }
     },
     Event: {
         screen: EventScreen,
         path: '/liveEvent/:name',
         navigationOptions: {
-            headerTitleStyle: {color: '#fff'},
-            header: (props) => <ImageHeader {...props} />,
+            ...defaultNavOptions,
+            title: "Event"
+
         }
     }
 });
@@ -56,17 +41,17 @@ const LiveTab = StackNavigator({
         screen: LiveEventsScreen,
         path: '/',
         navigationOptions: {
-            title: "Live",
-            headerTitleStyle: {color: '#fff'},
-            header: (props) => <ImageHeader {...props} />,
+            ...defaultNavOptions,
+            title: "Live"
         }
     },
     Event: {
         screen: EventScreen,
         path: '/liveEvent/:name',
-        navigationOptions: ({navigation}) => ({
-            title: `EVENT`
-        })
+        navigationOptions: {
+            ...defaultNavOptions,
+            title: "Event"
+        }
     }
 });
 
@@ -75,16 +60,17 @@ const SportsTab = StackNavigator({
         screen: SportScreen,
         path: '/',
         navigationOptions: {
-            title: 'SPORTS'
-
+            ...defaultNavOptions,
+            title: "Sports"
         }
     },
     Event: {
         screen: EventScreen,
         path: '/liveEvent/:name',
-        navigationOptions: ({navigation}) => ({
-            title: `${navigation.state.params.name}'s Event!`
-        })
+        navigationOptions: {
+            ...defaultNavOptions,
+            title: "Event"
+        }
     }
 });
 
@@ -93,15 +79,17 @@ const BetHistoryTab = StackNavigator({
         screen: BetHistoryScreen,
         path: '/n',
         navigationOptions: {
-            title: 'BETS'
+            ...defaultNavOptions,
+            title: "Bets"
         }
     },
     Event: {
         screen: EventScreen,
         path: '/liveEvent/:name',
-        navigationOptions: ({navigation}) => ({
-            title: `${navigation.state.params.name}'s Event!`
-        })
+        navigationOptions: {
+            ...defaultNavOptions,
+            title: "Event"
+        }
     }
 });
 
@@ -156,5 +144,27 @@ const App = TabNavigator({
             activeTintColor: Platform.select({ios: () => "#00ADC9", android: () => "white"})()
         }
     })
+
+function iconResolver(icon: string) {
+    return ({focused, tintColor}) => {
+        let iconName = icon;
+        if (!focused) {
+            iconName += "-outline"
+        }
+        return <Icon name={iconName} size={30} color={tintColor}/>
+    }
+}
+
+const ImageHeader = props => (
+    <View style={{backgroundColor: '#eee'}}>
+        <Image
+            style={absoluteFill}
+            source={{uri: 'https://upload.wikimedia.org/wikipedia/commons/3/36/Hopetoun_falls.jpg'}}
+        />
+        <Header {...props} style={{backgroundColor: 'transparent'}}/>
+    </View>
+);
+
+StatusBar.setBarStyle("light-content")
 
 export default App
