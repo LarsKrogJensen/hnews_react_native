de191dda5544b85ff11453468c936bc23956761a
Lars Krog-Jensen <lars.krogjensen@gmail.com>
2017-11-21 20:42:51 +0100

    Starting with drawer sidebar.

--- src/app/Drawer.tsx
+++ src/app/Drawer.tsx
@@ -1,24 +1,27 @@
 import * as React from "react"
 import {NavigationScreenProp} from "react-navigation";
-import {Button, View} from "react-native";
+import {Image, StyleSheet, Text, View} from "react-native";
+import banner from "images/banner";
+import absoluteFill = StyleSheet.absoluteFill;
 
-interface DraweProps {
+interface DrawerProps {
     navigation: NavigationScreenProp<{}, {}>
 }
 
-export default class Drawer extends React.Component<DraweProps> {
+export default class Drawer extends React.Component<DrawerProps> {
 
     render() {
         return (
-            <View style={{marginTop: 64}}>
-                <Button
-                    onPress={() => this.props.navigation.navigate('Home')}
-                    title="Go to Home"
-                />
-                <Button
-                    onPress={() => this.props.navigation.navigate('Live')}
-                    title="Go to Live"
-                />
+            <View style={{backgroundColor: "#333333", flexDirection: "column"}}>
+                <View style={{height: 100, justifyContent: "center", alignItems: "center"}}>
+                    <Image style={absoluteFill}
+                           source={{uri: banner}}
+                    />
+                    <Text style={{color: "white", fontSize: 24, fontWeight: "bold"}}>PLAY</Text>
+                </View>
+                <View style={{flex: 1, backgroundColor: "#333333"}}>
+                    <Text> Yes</Text>
+                </View>
             </View>
         )
     }
