17974f729020b424d9987f2d95cfc8fe5c98ffbf
Lars Krog-Jensen <lars.krogjensen@gmail.com>
2018-02-16 18:48:14 +0100

    switch to SE market

--- src/store/API.ts
+++ src/store/API.ts
@@ -2,7 +2,7 @@ export const API = {
     host: "https://e4-api.kambi.com",
     push: "wss://e1-push.aws.kambicdn.com",
     offering: "ub",
-    market:  "GB",
+    market:  "SE",
     lang: "en_GB",
     pushLang: "en"
 }
